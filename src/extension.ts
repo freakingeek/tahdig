0a1cb6f7dca0d8eca282bd7654b61b9aa0b2f1d6
Yasin Silavi <ys.silavi@gmail.com>
2023-09-17 03:26:34 +0330

    refactor: improve base functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 import * as vscode from "vscode";
-import * as commands from "./commands";
-import * as functions from "./functions";
+import { Token } from "./types/user";
+// import * as commands from "./commands";
+import { handleUserToken } from "./utils/user";
+import { fetchTodaysLunch } from "./apis/fetchTodaysLunch";
 
 // Bottom menu in vscode
 const statusBar = vscode.window.createStatusBarItem(
@@ -8,26 +10,26 @@ const statusBar = vscode.window.createStatusBarItem(
 );
 
 export async function activate(context: vscode.ExtensionContext) {
-  const apiKey: ApiKey = await functions.handleUserApiKey();
-  const lunch: string = await functions.getTodayLunch(apiKey);
+  const token: Token = await handleUserToken();
+  const lunch = await fetchTodaysLunch(token);
 
   statusBar.text = lunch || "!ناهار نداریم";
   statusBar.show();
 
-  functions.remindReserveLunch();
+  // functions.remindReserveLunch();
 
   // Commands
-  let refreshApi = vscode.commands.registerCommand(
-    "basalam-tahdig.refresh_api",
-    commands.refreshLunchApi.bind({}, statusBar, apiKey)
-  );
+  // let refreshApi = vscode.commands.registerCommand(
+  //   "basalam-tahdig.refresh_api",
+  //   commands.refreshLunchApi.bind({}, statusBar, apiKey)
+  // );
 
-  const changeApiKey = vscode.commands.registerCommand(
-    "basalam-tahdig.change_api",
-    commands.changeApiKey
-  );
+  // const changeApiKey = vscode.commands.registerCommand(
+  //   "basalam-tahdig.change_api",
+  //   commands.changeApiKey
+  // );
 
-  context.subscriptions.push(refreshApi, changeApiKey);
+  // context.subscriptions.push(refreshApi, changeApiKey);
 }
 
 // export function deactivate() {}
