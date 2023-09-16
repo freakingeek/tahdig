928e69aee18201d0846de23835935a0b8fd4b5a1
Yasin Silavi <ys.silavi@gmail.com>
2023-09-17 02:18:34 +0330

    feat: create a separate file for project constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 import { homedir } from "os";
 import { join } from "path";
 
-export const CONFIG_FILE_PATH = join(homedir(), ".tahdig.cfg");
+export const CONFIG_FILE_PATH = join(homedir(), ".tahdig");
